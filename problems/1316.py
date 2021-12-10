6ac2a7a64ef02b3185047644bd0f488e6924618d
DevRuby <hiveruby@gmail.com>
2021-12-11 08:39:18 +0900

    1316 - 그룹 단어 체커

---
+++ problems/1316.py
@@ -0,0 +1,27 @@
+# 1316 - 그룹 단어 체커
+
+def solve():
+    N = int(input())
+    count = 0
+
+    for i in range(N):
+        alphabet = list("abcdefghijklmnopqrstuvwxyz")
+        word = input()
+        cur = 1
+        for w in range(len(word)):
+            if w == 0:
+                alphabet.remove(word[w])
+                continue
+            else:
+                if word[w] in alphabet:
+                    alphabet.remove(word[w])
+                    continue
+                else:
+                    if word[w-1] == word[w]:
+                        continue
+                    else:
+                        cur = 0
+        count+=cur
+    print(count)
+
+solve()
