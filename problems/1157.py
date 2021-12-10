279ec9a13849d54c46eda09817ed94d30b480d4c
DevRuby <hiveruby@gmail.com>
2021-12-10 23:23:57 +0900

    1157 - 단어 공부

---
+++ problems/1157.py
@@ -0,0 +1,17 @@
+# 1157 - 단어 공부
+
+def solve():
+    word = input().upper()
+    alphabet = "abcdefghijklmnopqrstuvwxyz".upper()
+    count = 0
+    M = ""
+
+    for a in alphabet:
+        if count == word.count(a):
+            M = "?"
+        if count < word.count(a):
+            count = word.count(a)
+            M = a
+    print(M)
+
+solve()
