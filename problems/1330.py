d743b06bebfa011a105ffebbc5dc2bc134707b13
DevRuby <hiveruby@gmail.com>
2021-12-11 08:41:21 +0900

    1330 - 두 수 비교하기

---
+++ problems/1330.py
@@ -0,0 +1,12 @@
+# 1330 - 두 수 비교하기
+
+def solve():
+    a, b = map(int, input().split())
+    if a == b:
+        print("==")
+    elif a < b:
+        print("<")
+    elif a > b:
+        print(">")
+
+solve()
