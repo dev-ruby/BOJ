5178fa3e585bcf23652640770122a902750e4e72
DevRuby <hiveruby@gmail.com>
2021-12-11 08:38:41 +0900

    1193 - 분수찾기

--- problems/1193.py
+++ problems/1193.py
@@ -1,22 +1,25 @@
 # 1193 - 분수찾기
 
-N = int(input())
-n = 1
-a = 0
-b = 0
+def solve():
+    N = int(input())
+    n = 1
+    a = 0
+    b = 0
 
-while a < N:
-    a = n * (n+1) / 2
-    if a >= N: break
-    n+=1
+    while a < N:
+        a = n * (n+1) / 2
+        if a >= N: break
+        n+=1
 
 
-a = 1 + (n * (n+1) / 2 - N)
-b = n - (n * (n+1) / 2 - N)
+    a = 1 + (n * (n+1) / 2 - N)
+    b = n - (n * (n+1) / 2 - N)
 
-if n%2==0:
-    a, b = b, a
+    if n%2==0:
+        a, b = b, a
 
-a, b = map(int, (a,b))
+    a, b = map(int, (a,b))
 
-print(f"{a}/{b}")
+    print(f"{a}/{b}")
+
+solve()
