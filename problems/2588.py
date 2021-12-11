98655f33003624c06b04e3a1417a847bd65853b7
DevRuby <hiveruby@gmail.com>
2021-12-11 16:02:44 +0900

    formatting

--- problems/2588.py
+++ problems/2588.py
@@ -4,12 +4,13 @@
 def solve():
     a = int(input())
     b = int(input())
-    
+
     bl = list(str(b))
 
     print(int(bl[-1]) * a)
     print(int(bl[-2]) * a)
     print(int(bl[-3]) * a)
     print(a * b)
+
 
 solve()
