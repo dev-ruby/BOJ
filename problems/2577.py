92ca7f55d5528a7c3aed945843ad815f8933c5db
DevRuby <hiveruby@gmail.com>
2021-12-11 15:58:25 +0900

    2577 - 숫자의 개수

---
+++ problems/2577.py
@@ -0,0 +1,12 @@
+# 2577 - 숫자의 개수
+
+
+def solve():
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    abc = list(str(a * b * c))
+    for i in range(0, 10):
+        print(abc.count(str(i)))
+
+solve()
