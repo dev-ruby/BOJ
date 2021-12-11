70cda6f7d463b8d0310b6262156b94c348375eff
DevRuby <hiveruby@gmail.com>
2021-12-11 09:13:49 +0900

    formatting

--- problems/2292.py
+++ problems/2292.py
@@ -1,13 +1,15 @@
-# 2292 - 벌집 
+# 2292 - 벌집
+
 
 def solve():
     N = int(input())
 
     r = 1
     c = 1
-    while N > r :
+    while N > r:
         r += 6 * c
         c += 1
     print(c)
+
 
 solve()
