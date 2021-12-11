12909aa147274b304b87a1da0614ffe74c03e4db
DevRuby <hiveruby@gmail.com>
2021-12-11 16:00:54 +0900

    2588 - 곱셈

---
+++ problems/2588.py
@@ -0,0 +1,15 @@
+# 2588 - 곱셈
+
+
+def solve():
+    a = int(input())
+    b = int(input())
+    
+    bl = list(str(b))
+
+    print(int(bl[-1]) * a)
+    print(int(bl[-2]) * a)
+    print(int(bl[-3]) * a)
+    print(a * b)
+
+solve()
