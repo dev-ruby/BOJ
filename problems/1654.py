f41692d53b250b87d7149fb334a388be58d2c9c4
DevRuby <hiveruby@gmail.com>
2021-12-11 19:15:33 +0900

    1654 - 랜선 자르기

---
+++ problems/1654.py
@@ -0,0 +1,24 @@
+# 1654 - 랜선 자르기
+
+import sys
+
+
+def solve():
+    K, N = map(int, input().split())
+    n_list = list()
+    for _ in range(K):
+        n_list.append(int(sys.stdin.readline().strip()))
+
+    high, low = sum(n_list) // N, 1  # 각 랜선의 최소/최대 길이
+    while low <= high:
+        mid = (high + low) // 2  # 최소/최대 중간값
+        count = sum([x // mid for x in n_list])
+        if count < N:
+            high = mid - 1
+        elif count >= N:
+            low = mid + 1
+            ans = mid
+    print(ans)
+
+
+solve()
