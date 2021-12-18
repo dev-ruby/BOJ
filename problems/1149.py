2d2d29970f55597f4e353ae1216fb12b32571ce7
DevRuby <hiveruby@gmail.com>
2021-12-18 12:59:48 +0900

    1149 - RGB거리

---
+++ problems/1149.py
@@ -0,0 +1,21 @@
+# 1149 - RGB거리
+
+import sys
+
+
+def solve():
+    N = int(input())
+    n_list = list()
+
+    for _ in range(N):
+        n_list.append(list(map(int, sys.stdin.readline().split())))
+
+    for i in range(1, N):
+        n_list[i][0] = min(n_list[i - 1][1], n_list[i - 1][2]) + n_list[i][0]
+        n_list[i][1] = min(n_list[i - 1][2], n_list[i - 1][0]) + n_list[i][1]
+        n_list[i][2] = min(n_list[i - 1][1], n_list[i - 1][0]) + n_list[i][2]
+
+    print(min(n_list[N - 1]))
+
+
+solve()
